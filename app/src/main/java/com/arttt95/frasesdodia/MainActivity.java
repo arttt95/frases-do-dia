fdb685b38328672378c479e732fdd1412e764c09
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-11-27 15:51:28 -0300

    27NOV24 - Aula 38 AS - Finalizando #App Frases do Dia (frase random() setText()).

--- app/src/main/java/com/arttt95/frasesdodia/MainActivity.java
+++ app/src/main/java/com/arttt95/frasesdodia/MainActivity.java
@@ -1,6 +1,8 @@
 package com.arttt95.frasesdodia;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Atributos
+    String[] frases = {
+            "Seja modesto nas suas expectativas e econômico nas suas necessidades.",
+            "O trabalho dignifica o homem.",
+            "Viver sem liberdade é pior que a própria morte.",
+            "E que alegria seria morrer pelo Brasil!"
+    };
+
+    /*
+    textResultado, buttonResultado, editResultado
+    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +38,31 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void gerarFrase(View view) {
+
+        TextView texto = findViewById(R.id.textResultado);
+
+        int numeroAleatorio = new Random().nextInt(4); // 0, 1, 2, 3
+
+        String frase = frases[numeroAleatorio];
+
+        texto.setText( frase );
+
+    }
+
+    public void exibirTodas(View view) {
+
+        TextView texto = findViewById(R.id.textResultado);
+
+        String textoResultado = "";
+
+        for ( String frase : frases ) {
+            textoResultado = textoResultado + frase + "\n\n";
+        }
+
+        texto.setText( textoResultado );
+
+    }
+
 }
